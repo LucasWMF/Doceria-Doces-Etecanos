f69bf12c12787576f366be705dcf76544de542bd
Lucas Wagner de Melo Fogaça <141058064+LucasWMF@users.noreply.github.com>
2024-04-10 22:54:25 -0300

    Small modifications

---
+++ assents/JavaScript/main.js
@@ -0,0 +1,49 @@
+// EventListener's Front
+
+window.addEventListener('scroll', function () {
+    let header = document.getElementsByClassName('main-header');
+
+    if (window.scrollY > 100) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+});
+
+// EventListener's Back
+
+window.addEventListener("beforeunload", function (event) {
+    if (event.target === window) {
+        if (connected === true) {
+            alert(`Sua conta foi reiniciada`);
+        }
+    }
+});
+
+document.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let loginButton = document.getElementById('submit-button-login');
+    let registrationForm = document.getElementById('registrationForm');
+
+    if (loginButton) {
+        login();
+    } else if (registrationForm) {
+        registred();
+    }
+});
+
+
+document.getElementById('user-access').addEventListener("click", () => {
+    abrirModal();
+});
+
+document.getElementById('exit-modal').addEventListener("click", () => {
+    fecharModal();
+});
+
+document.getElementsByClassName('container-modal').addEventListener("click", () => {
+    fecharModal();
+});
+
+
+// Variáveis Function
